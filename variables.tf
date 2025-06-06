8fff986a779ac29300bbaa1a0eb859046e966b85
Oleksandr Kuzminskyi <aleksandr.kuzminsky@gmail.com>
2025-06-06 14:44:08 -0700

    Fix contact type. Not a map of objects but an object

--- variables.tf
+++ variables.tf
@@ -1,35 +1,31 @@
 variable "primary_contact" {
   description = "Primary contact for the account."
-  type = map(
-    object(
-      {
-        address_line_1     = string
-        address_line_2     = optional(string, null)
-        address_line_3     = optional(string, null)
-        city               = string
-        company_name       = string
-        country_code       = string
-        district_or_county = optional(string, null)
-        full_name          = string
-        phone_number       = string
-        postal_code        = string
-        state_or_region    = optional(string, null)
-        website_url        = optional(string, null)
-      }
-    )
+  type = object(
+    {
+      address_line_1     = string
+      address_line_2     = optional(string, null)
+      address_line_3     = optional(string, null)
+      city               = string
+      company_name       = string
+      country_code       = string
+      district_or_county = optional(string, null)
+      full_name          = string
+      phone_number       = string
+      postal_code        = string
+      state_or_region    = optional(string, null)
+      website_url        = optional(string, null)
+    }
   )
 }
 
 variable "security_contact" {
   description = "Security contact for the account."
-  type = map(
-    object(
-      {
-        full_name    = string
-        phone_number = string
-        title        = string
-        email        = string
-      }
-    )
+  type = object(
+    {
+      full_name    = string
+      phone_number = string
+      title        = string
+      email        = string
+    }
   )
 }
