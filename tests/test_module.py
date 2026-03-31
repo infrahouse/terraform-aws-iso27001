65b1e23a1faac5eabd9c6e65efca50a8486e82f7
Oleksandr Kuzminskyi <aleks@infrahouse.com>
2026-03-31 10:48:42 -0700

    Add .checkov.yml to support `checkov -d . --config-file .checkov.yml`
    ... with justified skips for registry-based module pinning, the existing
    AdministratorAccess policy, and wildcard resources required for
    org-wide log retention enforcement.

--- tests/test_module.py
+++ tests/test_module.py
@@ -37,26 +37,16 @@ def test_module(
             pass
 
     with open(osp.join(terraform_module_dir, "terraform.tfvars"), "w") as fp:
-        fp.write(
-            dedent(
-                f"""
+        fp.write(dedent(f"""
                     region              = "{aws_region}"
-                    """
-            )
-        )
+                    """))
         if test_role_arn:
-            fp.write(
-                dedent(
-                    f"""
+            fp.write(dedent(f"""
                     role_arn        = "{test_role_arn}"
-                    """
-                )
-            )
+                    """))
 
     with open(osp.join(terraform_module_dir, "terraform.tf"), "w") as fp:
-        fp.write(
-            dedent(
-                f"""
+        fp.write(dedent(f"""
                 terraform {{
                   required_version = "~> 1.5"
                   //noinspection HILUnresolvedReference
@@ -67,9 +57,7 @@ def test_module(
                     }}
                   }}
                 }}
-                """
-            )
-        )
+                """))
 
     with terraform_apply(
         terraform_module_dir,
